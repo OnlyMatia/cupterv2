2b0c14b581620d58cba42a85abcf5cab42a056a8
OnlyMatia <sajin.matija2154@gmail.com>
2024-05-25 14:49:22 +0200

    edited css for contactForm inputs

--- src/assets/pages/Contact.tsx
+++ src/assets/pages/Contact.tsx
@@ -5,8 +5,8 @@ const Contact = () => {
     <section className="contact" >
 
       <div className="contactText">
-        <h2 className="section-title contact__initial">Pošaljite nam poruku</h2>
-        <p className="contact__description">Ako imate dodatnih pitanja u vezi ćuptera ili veleprodaje slobodno nas kontaktirajte putem email-a.</p>
+        <h2 className="section-title contact__initial">Kontaktirajte nas  </h2>
+        <p className="contact__description">Ako imate pitanja, trebate dodatne informacije ili želite napraviti narudžbu, slobodno nas kontaktirajte. <br /> Rado ćemo vam pomoći i odgovoriti što prije.</p>
       </div>
       
       <ContactForm />
